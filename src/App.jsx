3c41752d32eda45c385e769c4d8380d4c69d5cc9
Val <91499731+val-pivchenko@users.noreply.github.com>
2024-06-08 15:17:41 -0400

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="h-screen bg-gradient-to-br from-white to-[#D2D6DB] ">
       <Navbar />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,36 +8,48 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-row justify-between mx-4 mt-8">
-      <img src="abstractly.png" alt="logo" className="w-32" />
-      <button
-        onClick={handleClick}
-        className="flex flex-col justify-center items-center gap-0.5"
+    <>
+      <div
+        className={`flex flex-row justify-between px-4 pt-8 ${
+          isOpen ? "bg-white" : ""
+        }`}
       >
-        <span
-          className={`bg-neutral-600 block transition-all duration-300 ease-out h-0.5 w-4  ${
-            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
-          }`}
-        ></span>
-        <span
-          className={`bg-neutral-600 block transition-all duration-300 ease-out h-0.5 w-4 ${
-            isOpen ? "opacity-0" : "opacity-100"
-          }`}
-        ></span>
-        <span
-          className={`bg-neutral-600 block transition-all duration-300 ease-out   h-0.5 w-4  ${
-            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
-          }`}
-        ></span>
-      </button>
-      <ul className={isOpen ? "flex" : "hidden"}>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <img src="abstractly.png" alt="logo" className="w-32" />
+        <button
+          onClick={handleClick}
+          className="flex flex-col justify-center items-center gap-0.5"
+        >
+          <span
+            className={`bg-neutral-600 block transition-all duration-300 ease-out h-0.5 w-4  ${
+              isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+            }`}
+          ></span>
+          <span
+            className={`bg-neutral-600 block transition-all duration-300 ease-out h-0.5 w-4 ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }`}
+          ></span>
+          <span
+            className={`bg-neutral-600 block transition-all duration-300 ease-out   h-0.5 w-4  ${
+              isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+            }`}
+          ></span>
+        </button>
+      </div>
+      <ul
+        className={
+          isOpen
+            ? "flex flex-col bg-white pt-6 px-4 w-full h-full max-h-screen transition ease-in-out delay-50"
+            : "translate-x-1 hidden"
+        }
+      >
+        <li className="mt-2 py-2 px-3">Home</li>
+        <li className="mt-2 py-2 px-3">Features</li>
+        <li className="mt-2 py-2 px-3">Pricing</li>
+        <li className="mt-2 py-2 px-3">About Us</li>
+        <li className="mt-2 py-2 px-3">Contact</li>
       </ul>
-    </div>
+    </>
   );
 };
 
